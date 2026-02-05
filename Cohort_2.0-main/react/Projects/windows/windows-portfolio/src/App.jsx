1f85c3dcf8bb5d39ac895fde84aceae60fee8a78
piyushh-ai <thepiyushsirolia@gmail.com>
2026-02-05 22:19:32 +0530

    backend started

---
+++ Cohort_2.0-main/react/Projects/windows/windows-portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.scss"
+import Taskbar from './components/Taskbar'
+
+const App = () => {
+  return (
+    <main>
+      <Taskbar/>
+    </main>
+  )
+}
+
+export default App
---
+++ Cohort_2.0-main/react/Projects/windows/windows-portfolio/src/components/Taskbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Taskbar.scss";
+
+const Taskbar = () => {
+  return (
+    <div className="task h-full w-full">
+      <div className="left">dfdfd</div>
+      <div className="center">dfdf</div>
+      <div className="right">dfdsfs</div>
+    </div>
+  );
+};
+
+export default Taskbar;
