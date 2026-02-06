2c22ec13ea5eaa989010f5f0cef8559a9af4f518
piyushh-ai <thepiyushsirolia@gmail.com>
2026-02-06 22:39:21 +0530

    final

--- Cohort_2.0-main/Backend/NotesApp/frontend/src/App.jsx
+++ Cohort_2.0-main/Backend/NotesApp/frontend/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
 
   const fetcheAllNotes = () => {
-    axios.get("https://cohort-2-0-git-main-piyushh-ais-projects.vercel.app/api/notes").then((hihi) => {
+    axios.get("https://cohort-2-0-beta.vercel.app/api/notes").then((hihi) => {
       setNotes(hihi.data.notes);
     });
   };
@@ -18,7 +18,7 @@ const App = () => {
     e.preventDefault();
     const { title, description } = e.target.elements;
     axios
-      .post("https://cohort-2-0-git-main-piyushh-ais-projects.vercel.app/api/notes", {
+      .post("https://cohort-2-0-beta.vercel.app/api/notes", {
         title: title.value,
         description: description.value,
       })
@@ -31,7 +31,7 @@ const App = () => {
   };
 
   const deleteHandler = (id) => {
-    axios.delete("https://cohort-2-0-git-main-piyushh-ais-projects.vercel.app/api/notes/" + id).then((res) => {
+    axios.delete("https://cohort-2-0-beta.vercel.app/api/notes/" + id).then((res) => {
       console.log(res);
       fetcheAllNotes();
     });
