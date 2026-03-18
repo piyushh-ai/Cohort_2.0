f0bf6016a2886c01db2d710b5ebfdd4021d05a87
piyushh-ai <thepiyushsirolia@gmail.com>
2026-03-18 21:13:26 +0530

    dfsdf

--- Cohort_2.0-main/Backend/OopsMovies/frontend/src/services/api.js
+++ Cohort_2.0-main/Backend/OopsMovies/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://oopsmovies.onrender.com/api",
+  baseURL: "https://oopsmovies-t16j.onrender.com/api",
   withCredentials: true,
 });
 
