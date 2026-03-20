e20b8ae418472def9ab6730327101f31a0bf35d8
piyushh-ai <thepiyushsirolia@gmail.com>
2026-03-20 23:11:21 +0530

    mail service

--- Cohort_2.0-main/Backend/Perplexity/backend/src/services/mail.service.js
+++ Cohort_2.0-main/Backend/Perplexity/backend/src/services/mail.service.js
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail({ to, subject, html, text }) {
   const { data, error } = await resend.emails.send({
-    from: "Perplexity <onboarding@resend.dev>",
+    from: "Perplexity <noreply@perplexity-piyush.in>",
     to,
     subject,
     html,
