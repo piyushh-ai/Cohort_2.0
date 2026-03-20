4c4cd301fed779dd76a45d5f87ea523fe7e2ffd7
piyushh-ai <thepiyushsirolia@gmail.com>
2026-03-20 13:14:55 +0530

    Delete Cohort_2.0-main/Backend/Collectra directory

--- Cohort_2.0-main/Backend/Collectra/Backend/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js";
-import { connectToDb } from "./src/config/database.js";
-
-connectToDb()
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
